2805538e065efc297516d56b333c6fd3e5d0e7a9
tatyanabelous05-ops <tatyana.belous05@gmail.com>
2026-05-21 20:46:04 +0300

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
